62353197823c36f716d076937e15e76447edd373
sofia.alysandratou <sofia.alysandratou@superbo.ai>
2022-07-14 13:05:47 +0300

    show test

---
+++ debug/js/test.js
@@ -0,0 +1,7 @@
+function showTest() {
+    // var msisdn = Ayoba.getMsisdn();
+    document.write('test showing')
+    // document.getElementById("msisdnText").textContent = 'test showing'
+    console.log('test showing: ', 'test showing');
+    // return msisdn
+}
